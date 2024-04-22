d1b9093f36944da831aefbbd116959bbb600ef10
Bobby Prior <robert_prior@progressive.com>
2024-04-22 15:05:16 -0400

    skeleton of issues api

--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTrackerApi.Controllers.Issues;
 
@@ -9,5 +10,46 @@ public class Api : ControllerBase
     public async Task<ActionResult> GetTheIssuesAsync()
     {
         return Ok();
+    }
+
+    [HttpPost("/issues")]
+    public async Task<ActionResult> AddIssueAsync(
+        [FromBody] CreateIssueRequestModel request,
+        [FromServices] IValidator<CreateIssueRequestModel> validator)
+    {
+        var results = await validator.ValidateAsync(request);
+        if (results.IsValid)
+        {
+            return Ok(request);
+        }
+
+        return BadRequest(results.ToDictionary());
+
+    }
+}
+
+public record CreateIssueRequestModel
+{
+    public string Software { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
+
+public class CreateIssueRequestModelValidator : AbstractValidator<CreateIssueRequestModel>
+{
+    private readonly IReadOnlyList<string> _supportedSoftware = ["excel", "powerpoint", "word"];
+
+    public CreateIssueRequestModelValidator()
+    {
+        RuleFor(i => i.Description)
+            .NotEmpty()
+            .MaximumLength(1024);
+
+        RuleFor(i => i.Software)
+            .NotEmpty()
+            .Must(i =>
+            {
+                return _supportedSoftware.Any(s => s.Equals(i, StringComparison.InvariantCultureIgnoreCase));
+            })
+            .WithMessage("Unsupported Software. Good Luck!");
     }
 }
