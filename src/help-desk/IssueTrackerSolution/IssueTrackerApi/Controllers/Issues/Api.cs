b91a28111866ce3d90bb12752fece2f96e8299d4
Bobby Prior <robert_prior@progressive.com>
2024-04-22 13:49:55 -0400

    second commit

---
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IssueTrackerApi.Controllers.Issues;
+
+public class Api : ControllerBase
+{
+    // GET /issues
+    [HttpGet("/issues")]
+    public async Task<ActionResult> GetTheIssuesAsync()
+    {
+        return Ok();
+    }
+}
