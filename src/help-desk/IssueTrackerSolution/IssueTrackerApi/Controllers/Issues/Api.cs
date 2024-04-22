de0e72dab5c9d0ed718fd7b705cefdb4bc174548
Bobby Prior <robert_prior@progressive.com>
2024-04-22 16:05:44 -0400

    before support

--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Controllers/Issues/Api.cs
@@ -1,15 +1,31 @@
 ﻿using FluentValidation;
+using Marten;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IssueTrackerApi.Controllers.Issues;
 
-public class Api : ControllerBase
+public class Api(IDocumentSession session) : ControllerBase
 {
     // GET /issues
     [HttpGet("/issues")]
     public async Task<ActionResult> GetTheIssuesAsync()
     {
-        return Ok();
+        var issues = await session.Query<Issue>().ToListAsync();
+
+        return Ok(issues);
+    }
+
+    [HttpGet("/issues/{id:guid}")]
+    public async Task<ActionResult> GetIssueByIdAsync(Guid id)
+    {
+        var issue = await session.Query<Issue>().SingleOrDefaultAsync(i => i.Id == id);
+
+        if (issue is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(issue);
     }
 
     [HttpPost("/issues")]
@@ -20,12 +36,38 @@ public class Api : ControllerBase
         var results = await validator.ValidateAsync(request);
         if (results.IsValid)
         {
-            return Ok(request);
+            var response = new Issue
+            {
+                CreatedAt = DateTimeOffset.UtcNow,
+                Description = request.Description,
+                Software = request.Software,
+                Id = Guid.NewGuid(),
+                Status = IssueStatus.Created
+            };
+
+            session.Store(response);
+            await session.SaveChangesAsync();
+
+            return Ok(response);
         }
 
         return BadRequest(results.ToDictionary());
 
     }
+}
+
+public record Issue
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Software { get; set; } = string.Empty;
+    public DateTimeOffset CreatedAt { get; set; }
+    public IssueStatus Status { get; set; }
+}
+
+public enum IssueStatus
+{
+    Created
 }
 
 public record CreateIssueRequestModel
--- src/help-desk/IssueTrackerSolution/IssueTrackerApi/Program.cs
+++ src/help-desk/IssueTrackerSolution/IssueTrackerApi/Program.cs
@@ -1,15 +1,26 @@
 using FluentValidation;
 using IssueTrackerApi.Controllers.Issues;
+using Marten;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var databaseConnectionString = builder.Configuration.GetConnectionString("data") ?? throw new Exception("No Connection String");
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateIssueRequestModelValidator>();
+builder.Services.AddMarten(options =>
+{
+    options.Connection(databaseConnectionString);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
